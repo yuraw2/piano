0e77da9371621d4b55370a7e9452b3ac18488daf
yuraw2 <162898441+yuraw2@users.noreply.github.com>
2024-05-29 22:39:50 +0300

    Update Form1.cs

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,27 +14,27 @@ namespace PianoTiles
 {
     public partial class Form1 : Form
     {
-        public int[,] map = new int[8, 4];
-        public int cellWidth = 50;
+        public int[,] map = new int[8, 4];          //двумерный массив выступающий в виде карты
+        public int cellWidth = 50;                 //Переменные отвечающии за ширину и высоту клетки
         public int cellHeight = 80;
 
         public Form1()
         {
             InitializeComponent();
 
-            this.Text = "Piano";
-            this.Width = cellWidth * 4 + 15;
+            this.Text = "Piano";                   //имя формы
+            this.Width = cellWidth * 4 + 15;           //ширина и длина формы
             this.Height = cellHeight * 8 + 40;
             this.Paint += new PaintEventHandler(Repaint);
-            this.KeyUp += new KeyEventHandler(OnKeyboardPressed);
-            Init();
+            this.KeyUp += new KeyEventHandler(OnKeyboardPressed);           //привязка обработчика на событие кейап
+            Init();                   //старт проекта
         }
 
-        private void OnKeyboardPressed(object sender, KeyEventArgs e)
+        private void OnKeyboardPressed(object sender, KeyEventArgs e)               //функция обрабатывания нажатия наших клавиш, добавления управления в нашу игру
         {
-            switch (e.KeyCode.ToString())
+            switch (e.KeyCode.ToString())                  //через свитч проверяем какую клавишу нажали
             {
-                case "D1":
+                case "D1":                              //1 на клаве
                     CheckForPressedButton(0);
                     break;
                 case "D2":
@@ -49,7 +49,7 @@ namespace PianoTiles
             }
         }
 
-        public void CheckForPressedButton(int i)
+        public void CheckForPressedButton(int i)            //вспомогательная функция, в которую будем передовать i, то есть нажату кнопку
         {
             if (map[7, i] != 0)
             {
@@ -58,7 +58,7 @@ namespace PianoTiles
             }
             else
             {
-                MessageBox.Show("You lost!");
+                MessageBox.Show("ТЫ ПРОДУЛ!");
                 Init();
             }
         }
@@ -68,7 +68,7 @@ namespace PianoTiles
             System.IO.Stream str = null;
             switch (sound)
             {
-                case 0:
+                case 0:                  //если пришло 0, тоесть число 1 по клавише, то вызываем звук
                     str = Resources.g6;
                     break;
                 case 1:
@@ -81,7 +81,7 @@ namespace PianoTiles
                     str = Resources.e6;
                     break;
             }
-            System.Media.SoundPlayer snd = new System.Media.SoundPlayer(str);
+            System.Media.SoundPlayer snd = new System.Media.SoundPlayer(str);         //проигрывание выбранного звука
             snd.Play();
         }
 
@@ -91,19 +91,19 @@ namespace PianoTiles
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    map[i, j] = map[i - 1, j];
+                    map[i, j] = map[i - 1, j];        //каждый элемент смещаем вниз
                 }
             }
-            AddNewLine();
-            Invalidate();
+            AddNewLine(); 
+            Invalidate();           //перерисовка
         }
 
-        public void AddNewLine()
+        public void AddNewLine()          //добавления новой линии
         {
             Random r = new Random();
             int j = r.Next(0, 4);
             for (int k = 0; k < 4; k++)
-                map[0, k] = 0;
+                map[0, k] = 0;         //вверхнюю строку полностью зануляем, потому что при смещении она скопировалась, занулировали чтобы она была пустая, а затем в эту 0 строку уже с j-ой рандомной переменной закидываем 1
             map[0, j] = 1;
         }
 
@@ -111,10 +111,10 @@ namespace PianoTiles
         {
             ClearMap();
             GenerateMap();
-            Invalidate();
+            Invalidate();          //фунция инвалидейт чтобы выхвалось перересовка нашей формы
         }
 
-        public void ClearMap()
+        public void ClearMap() 
         {
             for (int i = 0; i < 8; i++)
             {
@@ -125,13 +125,13 @@ namespace PianoTiles
             }
         }
 
-        public void GenerateMap()
+        public void GenerateMap()            //создание начальной карты
         {
             Random r = new Random();
             for (int i = 0; i < 8; i++)
             {
-                int j = r.Next(0, 4);
-                map[i, j] = 1;
+                int j = r.Next(0, 4);          //в этой переменной записывается рандомное число от 0 до 3
+                map[i, j] = 1;          // 1 значит клетка будет черная
             }
         }
 
@@ -141,30 +141,30 @@ namespace PianoTiles
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    if (map[i, j] == 0)
+                    if (map[i, j] == 0)        // если элемен карты равен 0
                     {
-                        g.FillRectangle(new SolidBrush(Color.White), cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+                        g.FillRectangle(new SolidBrush(Color.White), cellWidth * j, cellHeight * i, cellWidth, cellHeight);     //то в этом месте с этими определенными координатами i j будем заполнять прямоугольник  белого цвета, далее координаты
                     }
-                    if (map[i, j] == 1)
+                    if (map[i, j] == 1)            // а если 1, то заполняем прямоугольник черным цветом
                     {
                         g.FillRectangle(new SolidBrush(Color.Black), cellWidth * j, cellHeight * i, cellWidth, cellHeight);
                     }
                 }
             }
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < 8; i++)                   // два цикла с помощью которых отрисуем вертикальные и горизонтальные линии для контуров карты, сперва 8 горизонтальных линий черного цвета с опр. координатами, по х от 0 до 4*ширину клетки, по у от i*ширину клетки до i высоту
             {
                 g.DrawLine(new Pen(new SolidBrush(Color.Black)), 0, i * cellHeight, 4 * cellWidth, i * cellHeight);
             }
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 4; i++)                 // 4 вертткалььные линии черного цвета, 
             {
                 g.DrawLine(new Pen(new SolidBrush(Color.Black)), i * cellWidth, 0, i * cellWidth, 8 * cellHeight);
             }
         }
 
-        private void Repaint(object sender, PaintEventArgs e)
+        private void Repaint(object sender, PaintEventArgs e)                   //функция репеинт которую мы будем привязывать на событие пэинт нашей формы
         {
-            Graphics g = e.Graphics;
-            DrawMap(g);
+            Graphics g = e.Graphics;           //графика из аргументиов
+            DrawMap(g);                       //вызываем функцию драймэп с переданным аргументов графики
         }
     }
 }
